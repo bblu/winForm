c665c4b07139d720b659e18be486a8baf458513b
wenbo.Lu <gmwblu@gmail.com>
2016-12-12 16:49:16 +0800

    upload soundPlayer

---
+++ soundPlayer/FormMain.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace winApp
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.splitContainer2 = new System.Windows.Forms.SplitContainer();
+            this.textQuestion = new System.Windows.Forms.RichTextBox();
+            this.splitContainer3 = new System.Windows.Forms.SplitContainer();
+            this.imageBox = new System.Windows.Forms.PictureBox();
+            this.splitCheck = new System.Windows.Forms.SplitContainer();
+            this.checkedListBox0 = new System.Windows.Forms.CheckedListBox();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).BeginInit();
+            this.splitContainer2.Panel1.SuspendLayout();
+            this.splitContainer2.Panel2.SuspendLayout();
+            this.splitContainer2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).BeginInit();
+            this.splitContainer3.Panel1.SuspendLayout();
+            this.splitContainer3.Panel2.SuspendLayout();
+            this.splitContainer3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.splitCheck)).BeginInit();
+            this.splitCheck.Panel1.SuspendLayout();
+            this.splitCheck.Panel2.SuspendLayout();
+            this.splitCheck.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.listBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.splitContainer2);
+            this.splitContainer1.Size = new System.Drawing.Size(740, 468);
+            this.splitContainer1.SplitterDistance = 187;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listBox1.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "第一关：小脑幕切迹疝",
+            "第二关：枕骨大孔疝"});
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(187, 468);
+            this.listBox1.TabIndex = 0;
+            // 
+            // splitContainer2
+            // 
+            this.splitContainer2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer2.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer2.Name = "splitContainer2";
+            this.splitContainer2.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer2.Panel1
+            // 
+            this.splitContainer2.Panel1.Controls.Add(this.textQuestion);
+            // 
+            // splitContainer2.Panel2
+            // 
+            this.splitContainer2.Panel2.Controls.Add(this.splitContainer3);
+            this.splitContainer2.Size = new System.Drawing.Size(549, 468);
+            this.splitContainer2.SplitterDistance = 161;
+            this.splitContainer2.TabIndex = 0;
+            // 
+            // textQuestion
+            // 
+            this.textQuestion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.textQuestion.Font = new System.Drawing.Font("宋体", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.textQuestion.Location = new System.Drawing.Point(0, 0);
+            this.textQuestion.Name = "textQuestion";
+            this.textQuestion.Size = new System.Drawing.Size(549, 161);
+            this.textQuestion.TabIndex = 0;
+            this.textQuestion.Text = "";
+            // 
+            // splitContainer3
+            // 
+            this.splitContainer3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer3.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer3.Name = "splitContainer3";
+            this.splitContainer3.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer3.Panel1
+            // 
+            this.splitContainer3.Panel1.Controls.Add(this.imageBox);
+            this.splitContainer3.Panel1.Controls.Add(this.splitCheck);
+            // 
+            // splitContainer3.Panel2
+            // 
+            this.splitContainer3.Panel2.Controls.Add(this.button1);
+            this.splitContainer3.Size = new System.Drawing.Size(549, 303);
+            this.splitContainer3.SplitterDistance = 245;
+            this.splitContainer3.TabIndex = 0;
+            // 
+            // imageBox
+            // 
+            this.imageBox.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.imageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.imageBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imageBox.Location = new System.Drawing.Point(0, 0);
+            this.imageBox.Name = "imageBox";
+            this.imageBox.Size = new System.Drawing.Size(549, 245);
+            this.imageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.imageBox.TabIndex = 1;
+            this.imageBox.TabStop = false;
+            this.imageBox.Click += new System.EventHandler(this.imageBox_Click);
+            // 
+            // splitCheck
+            // 
+            this.splitCheck.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitCheck.Location = new System.Drawing.Point(0, 0);
+            this.splitCheck.Name = "splitCheck";
+            // 
+            // splitCheck.Panel1
+            // 
+            this.splitCheck.Panel1.Controls.Add(this.checkedListBox0);
+            // 
+            // splitCheck.Panel2
+            // 
+            this.splitCheck.Panel2.Controls.Add(this.checkedListBox1);
+            this.splitCheck.Size = new System.Drawing.Size(549, 245);
+            this.splitCheck.SplitterDistance = 259;
+            this.splitCheck.TabIndex = 0;
+            // 
+            // checkedListBox0
+            // 
+            this.checkedListBox0.CheckOnClick = true;
+            this.checkedListBox0.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox0.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkedListBox0.FormattingEnabled = true;
+            this.checkedListBox0.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox0.Name = "checkedListBox0";
+            this.checkedListBox0.Size = new System.Drawing.Size(259, 245);
+            this.checkedListBox0.TabIndex = 0;
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.CheckOnClick = true;
+            this.checkedListBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.checkedListBox1.Font = new System.Drawing.Font("微软雅黑", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(0, 0);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(286, 245);
+            this.checkedListBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.button1.Font = new System.Drawing.Font("微软雅黑", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(549, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "提交";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(740, 468);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "FormMain";
+            this.Text = "闯关测试";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.splitContainer2.Panel1.ResumeLayout(false);
+            this.splitContainer2.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer2)).EndInit();
+            this.splitContainer2.ResumeLayout(false);
+            this.splitContainer3.Panel1.ResumeLayout(false);
+            this.splitContainer3.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer3)).EndInit();
+            this.splitContainer3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox)).EndInit();
+            this.splitCheck.Panel1.ResumeLayout(false);
+            this.splitCheck.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitCheck)).EndInit();
+            this.splitCheck.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.SplitContainer splitContainer2;
+        private System.Windows.Forms.RichTextBox textQuestion;
+        private System.Windows.Forms.SplitContainer splitContainer3;
+        private System.Windows.Forms.SplitContainer splitCheck;
+        private System.Windows.Forms.CheckedListBox checkedListBox0;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox imageBox;
+    }
+}
+
---
+++ soundPlayer/FormMain.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+namespace winApp
+{
+    public partial class FormMain : Form
+    {
+        string[] titles = { "第一关：小脑幕切迹疝", "第二关：枕骨大孔疝" };
+        string[] questions={ "识记：疝入组织：颞叶沟回\n      疝入孔道：小脑幕裂空（切迹）\n      疝入后果：受压脑组织缺血、水肿；动眼神经受挤压可产生动眼神经麻痹，造成瞳孔先小后大" +
+                "，最后散大；同侧大脑脚受挤压造成对侧肢体瘫痪\n\n练习：小脑幕切迹疝可能有哪些表现，多选",
+                "识记：疝入组织：小脑扁桃体\n      疝入孔道：枕骨大孔\n      疝入后果：直接压迫脑干，呼吸抑制出现较早\n\n练习：枕骨大孔疝可能有哪些表现，多选"};
+
+        string[] items ={
+                  "同侧瞳孔先小后大,对侧瞳孔先小后大,同侧瞳孔大小不定,对侧瞳孔大小不定,双侧瞳孔散大,进行性意识障碍;意识始终清醒,呼吸突然停止,同侧肢体瘫痪,对侧肢体瘫痪,双侧肢体瘫痪,有喷射性呕吐",
+                  "同侧瞳孔先小后大,对侧瞳孔先小后大,瞳孔无明显变化,进行性意识障碍,意识清醒;呼吸突然停止,呼吸停止晚于意识障碍,呼吸停止早于意识障碍,无肢体瘫痪,有喷射性呕吐" };
+        string[] answers ={ 
+            "同侧瞳孔先小后大,进行性意识障碍,对侧肢体瘫痪,有喷射性呕吐",
+        "瞳孔无明显变化,意识清醒,呼吸突然停止,呼吸停止早于意识障碍,无肢体瘫痪,有喷射性呕吐"
+        };
+        int[] ac={4,6};
+        int curIndex = 0;
+        SoundPlayer[] sounds={null,null};
+        public FormMain()
+        {
+            InitializeComponent();
+            imageBox.Visible = false;            
+            loadQuestion(0);
+            sounds[0] = new SoundPlayer(winApp.Properties.Resources.app);
+            sounds[0].Load();
+            sounds[1] = new SoundPlayer(winApp.Properties.Resources.sha);
+            sounds[1].Load();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            imageBox.Visible = button1.Text == "提交";
+            
+            if (button1.Text == "重新开始")
+            {
+                loadQuestion(0);                
+            }
+            else if (button1.Text == "下一题")
+            {
+                if (curIndex == 2)
+                    button1.Text = "闯关成功";
+                else
+                    loadQuestion(curIndex+1);              
+            }
+            else if(button1.Text == "提交")
+            {
+                if (isRight())
+                {
+                    imageBox.Image = winApp.Properties.Resources.img_0;
+                    sounds[0].Play();    
+                    if(curIndex<1)
+                        button1.Text = "下一题";
+                    else 
+                        button1.Text = "闯关成功";
+                }
+                else
+                {
+                    imageBox.Image = winApp.Properties.Resources.img_1;
+                    button1.Text = "重新开始";
+                    sounds[1].Play();
+                }
+               
+            }
+            else if (button1.Text == "闯关成功")
+            {
+                button1.Text = "重新开始";
+            }
+        }
+        private bool isRight()
+        {
+            System.Collections.ArrayList list = new ArrayList(10);        
+            list.AddRange(checkedListBox0.CheckedItems);
+            list.AddRange(checkedListBox1.CheckedItems);
+            if(list.Count != ac[curIndex])
+                return false;
+            foreach(object obj in list)
+            {
+                if(answers[curIndex].IndexOf(obj.ToString())<0)
+                    return false;
+            }
+            return true;
+        }
+        private void loadQuestion(int i)
+        {
+            curIndex = i;
+            listBox1.SelectedIndex = i;
+            string str = questions[i];
+            textQuestion.Clear();
+            textQuestion.AppendText(str);
+            string[] strItems = items[i].Split(';');
+            checkedListBox0.Items.Clear();
+            checkedListBox1.Items.Clear();
+            checkedListBox0.Items.AddRange(strItems[0].Split(','));
+            checkedListBox1.Items.AddRange(strItems[1].Split(','));
+            button1.Text = "提交";
+            this.Text = titles[i];
+        }
+
+        private void imageBox_Click(object sender, EventArgs e)
+        {
+            if (button1.Text == "闯关成功")
+            {
+                loadQuestion(0);
+            }
+        }
+    }
+}
